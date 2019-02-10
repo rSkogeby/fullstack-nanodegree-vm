63ddb18f8a4f1833230599fd335955fd91a8ddd1
Richard Skogeby <richard.skogeby@outlook.com>
2019-02-10 11:32:07 +0000

    SQLalchemy initial setup

--- vagrant/crud/db_setup.py
+++ vagrant/crud/db_setup.py
@@ -1,5 +1,11 @@
 import sys
 
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
 
 def main():
     pass
@@ -7,3 +13,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+engine = create_engine(
+    'sqlite://restaurantmenu.db'
+)
